6e5a7b5fc2c64189cba515416d855a7ab18396a1
DSUN <dsun@hanmail.net>
2023-06-16 12:38:17 +0900

    PagingSize 동적설정

--- src/A003.razor.cs
+++ src/A003.razor.cs
@@ -26,10 +26,32 @@ namespace MetaFrm.Management.Razor
         internal CardWindowStatus CardWindowStatusInt = CardWindowStatus.Maximize;
         internal CardWindowStatus CardWindowStatusNumber = CardWindowStatus.Maximize;
         internal CardWindowStatus CardWindowStatusDatetime = CardWindowStatus.Maximize;
+
+        internal int? PagingSize = null;
         #endregion
 
 
         #region Init
+        /// <summary>
+        /// OnInitializedAsync
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task<Task> OnInitializedAsync()
+        {
+            try
+            {
+                if (this.JSRuntime != null)
+                {
+                    System.Drawing.Size browserDimension = await this.JSRuntime.InvokeAsync<System.Drawing.Size>("getDimensions", null);
+                    int? tmp = (browserDimension.Height - 210) / this.DataGridControl.HeaderHeight;
+                    this.PagingSize = tmp < 5 ? 5 : tmp;
+                }
+            }
+            catch (Exception) { }
+
+            return base.OnInitializedAsync();
+        }
+
         /// <summary>
         /// OnAfterRenderAsync
         /// </summary>
